ed35d568ab7aed27f861f852e94f72500e5750d2
Jason Fairchild <ammonjasonfairchild@gmail.com>
2024-11-18 13:32:15 -0700

    Update and add tests for /draw endpoint

--- cypress/API tests/deck/draw.cy.js
+++ cypress/API tests/deck/draw.cy.js
@@ -1,34 +1,94 @@
 describe("Deck of Cards API", () => {
-  let deckId;
-
-  before(() => {
-    cy.request("POST", "/new/shuffle/").then((response) => {
-      deckId = response.body.deck_id;
+  beforeEach("Create a deck", () => {
+    cy.request("POST", "/new").then((response) => {
+      cy.wrap(response.body.deck_id).as("deckId");
     });
   });
 
   it("should draw a single card from the deck", () => {
-    cy.request({
-      method: "GET",
-      url: `/${deckId}/draw/`,
-      qs: { count: 1 },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.cards).to.have.length(1);
-      expect(response.body.cards[0]).to.have.property("code");
-      expect(response.body.remaining).to.be.greaterThan(0);
+    cy.get("@deckId").then((deckId) => {
+      cy.request({
+        method: "GET",
+        url: `/${deckId}/draw/`,
+      }).then((response) => {
+        expect(response.body.success).to.eq(true);
+        expect(response.body.cards).to.have.length(1);
+        expect(response.body.cards[0].code).to.eq("AS");
+        expect(response.body.cards[0].suit).to.eq("SPADES");
+        expect(response.body.cards[0].value).to.eq("ACE");
+        expect(response.body.cards[0]).to.have.property("image");
+        expect(response.body.cards[0]).to.have.property("images");
+        expect(response.body.remaining).to.eq(51);
+      });
     });
   });
 
-  it("should draw 5 cards from the deck", () => {
-    cy.request({
-      method: "GET",
-      url: `/${deckId}/draw/`,
-      qs: { count: 5 },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.cards).to.have.length(5);
-      expect(response.body.remaining).to.eq(46);
+  it("should draw no cards from deck", () => {
+    cy.get("@deckId").then((deckId) => {
+      cy.request({
+        method: "GET",
+        url: `/${deckId}/draw/`,
+        qs: { count: 0 },
+      }).then((response) => {
+        expect(response.body.cards).to.be.empty;
+        expect(response.body.remaining).to.eq(52);
+      });
+    });
+  });
+
+  it("should draw all cards from the deck", () => {
+    cy.get("@deckId").then((deckId) => {
+      cy.request({
+        method: "GET",
+        url: `/${deckId}/draw/`,
+        qs: { count: 52 },
+      }).then((response) => {
+        expect(response.body.cards).to.have.length(52);
+        expect(response.body.remaining).to.eq(0);
+      });
+    });
+  });
+
+  it("should draw 52 cards from the deck and give error", () => {
+    cy.get("@deckId").then((deckId) => {
+      cy.request({
+        method: "GET",
+        url: `/${deckId}/draw/`,
+        qs: { count: 53 },
+      }).then((response) => {
+        expect(response.body.success).to.eq(false);
+        expect(response.body.error).to.eq("Not enough cards remaining to draw 53 additional");
+        expect(response.body.cards).to.have.length(52);
+        expect(response.body.remaining).to.eq(0);
+      });
+    });
+  });
+
+  it("should result in remaining cards minus negative count cards drawn", () => {
+    cy.get("@deckId").then((deckId) => {
+      cy.request({
+        method: "GET",
+        url: `/${deckId}/draw/`,
+        qs: { count: -1 },
+      }).then((response) => {
+        expect(response.body.success).to.eq(true);
+        // remaining - 1 cards drawn
+        expect(response.body.cards).to.have.length(51);
+        expect(response.body.remaining).to.eq(1);
+      });
+    });
+  });
+
+  it("should return 500 when passed an invalid query string", () => {
+    cy.get("@deckId").then((deckId) => {
+      cy.request({
+        method: "GET",
+        url: `/${deckId}/draw/`,
+        qs: { count: "a" },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(500);
+      });
     });
   });
 });
